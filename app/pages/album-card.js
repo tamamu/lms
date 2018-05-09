d7789014d6f69142423d0ccf662cd7d725770e39
Eddie <tamamu.1r1s@gmail.com>
2018-05-09 12:52:08 +0900

    Modify style

--- app/pages/album-card.js
+++ app/pages/album-card.js
@@ -2,20 +2,29 @@
 import React from 'react'
 
 const cardStyle = {
-  backgroundColor: "gray",
+  backgroundColor: "#37474F",
   color: "white",
   width: 240,
-  height: 180,
+  height: 160,
   border: "1px solid black"
-}
+};
+
+const titleStyle = {
+  fontSize: "1.2em"
+};
+
+const propStyle = {
+  display: "inline",
+  marginRight: "1em"
+};
 
 export default class AlbumCard extends React.Component {
   render() {
     return (
       <div album-id={this.props.albumId} artist-id={this.props.artistId} key={this.props.key} style={cardStyle}>
-        <p>{this.props.title}</p>
-        <p>{this.props.genre}</p>
-        <p>{this.props.year}</p>
+        <p style={titleStyle}>{this.props.title}</p>
+        <p style={propStyle}>{this.props.genre}</p>
+        <p style={propStyle}>{this.props.year}</p>
       </div>
     )
   }
